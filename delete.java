8ad94477b7286dbc272c8ab7d700df344c3524bf
bezawitsebsbe <167539304+bezawitsebsbe@users.noreply.github.com>
2024-05-13 16:01:59 +0300

    delete.java

--- delete.java
+++ delete.java
@@ -13,13 +13,6 @@ public class delete {
 		 }
 		}
 	}
-    public static void main(String[] args) {
-    	int[] array={3, 7, 1, 9, 4};
-    	delete d=new delete();
-    	d.deleteElement(array, 2);
-    	System.out.println(d);
-		
-
-	}
+  
 
 }
