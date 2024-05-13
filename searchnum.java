e6736e7debe5afea64d765a0f7236e527965ebe6
bezawitsebsbe <167539304+bezawitsebsbe@users.noreply.github.com>
2024-05-13 16:00:33 +0300

    searchnum.java

--- searchnum.java
+++ searchnum.java
@@ -1,9 +1,28 @@
 package question1;
+import java.util.Scanner;
 
 public class searchnum {
+	   public static void main(String[] args) {
+		        Scanner scanner = new Scanner(System.in);
+		        System.out.print("Enter number of elements in the sequence: ");
+		        int n = scanner.nextInt();
+		       int[] numbers = new int[n];
+		        System.out.println("Enter the elements of the sequence:");
+		        for (int i = 0; i < n; i++) {
+		            numbers[i] = scanner.nextInt();
+		        }
+		        System.out.print("Enter the number to be searched: ");
+		        int searchNumber = scanner.nextInt();
+		        
+		        int count = 0;
+		        for (int i = 0; i < n; i++) {
+		            if (numbers[i] == searchNumber) {
+		                count++;
+		                }
+		           }
+		        System.out.println("the amount of time the element found is:"+count);
+			
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
